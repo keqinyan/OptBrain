70f6578b0b0ff8d4b896eadbdc95fba6840a725b
keqinyan <keqinyan8@gmail.com>
2026-04-29 14:14:04 -0400

    Initial Commit

---
+++ OptBrain/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  OptBrain
+//
+//  Created by Keqin Yan on 4/29/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ OptBrainTests/OptBrainTests.swift
@@ -0,0 +1,19 @@
+//
+//  OptBrainTests.swift
+//  OptBrainTests
+//
+//  Created by Keqin Yan on 4/29/26.
+//
+
+import Testing
+@testable import OptBrain
+
+struct OptBrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
